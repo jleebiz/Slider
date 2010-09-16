3b44f28949355145368acab80e23202fb95ffd13
James Lee <james.lee@stiqr.com>
2010-09-16 00:31:02 -0700

    created slides obj and div moved slide divs into slides div

--- index.php
+++ index.php
@@ -1,23 +1,35 @@
 <html>
 	<head>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
 		<script src="slider.js"></script>
 		<script> 
 			$().ready(function(){
-				$(".tip").slider();
+				$(".slider").slider("slide");
 			});		
 		</script>
 	</head>
 
 	<body>
-		<div id="slides">
+		<div class="slider">
 			<div class="slide">
-			
+				asdf
 			</div>
 			<div class="slide">
-			
+				asfd
 			</div>
 			<div class="slide">
-			
+				asdf
+			</div>
+		</div>
+		<div class="slider">
+			<div class="slide">
+				2
+			</div>
+			<div class="slide">
+				2
+			</div>
+			<div class="slide">
+				2
 			</div>
 		</div>
 	</body>
--- slider.js
+++ slider.js
@@ -1,11 +1,21 @@
 (function($){
 	$.fn.slider = function(options){
 		
+		var defaults = {
+			main_container: "slides",
+			indv_container: "slide",
+		};
+
+		this.each(function(){
+			obj = $(this);
+		// append slides div to this container
+			obj.append("<div class='slides'>");
+			obj.find(".slide").appendTo(obj.find(".slides"));
+			console.log(obj.find('slides'));
+		
+		// append each of the slide div to slides div
+			obj.find('.slide').css({'float' : 'left'});
+		});
 	};
-	
-	this.each(function(){
-		obj = $(this);
-		console.log(obj);
-	});
 	
 })(jQuery);
